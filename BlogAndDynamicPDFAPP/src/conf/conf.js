ca897f0e445e7329e50011b5badf020ad2ae9c37
shivanimutkeNS <shivani.mutke@noovosoft.com>
2024-03-25 17:50:00 +0530

    update vercel api key

--- BlogAndDynamicPDFAPP/src/conf/conf.js
+++ BlogAndDynamicPDFAPP/src/conf/conf.js
@@ -6,6 +6,4 @@ const conf = {
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
     tinymceId:String(import.meta.env.VITE_APPWRITE_TINY_MCE_ID)
 }
-// there was a name issue with the import.meta.env.VITE_APPWRITE_URL, it was later fixed in debugging video
-
 export default conf
